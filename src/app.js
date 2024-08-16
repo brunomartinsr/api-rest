ef1f90ef9dd0e4d1e7a970fe04a5965b9fc60aa9
Bruno Martins <139308991+brunomartinsr@users.noreply.github.com>
2024-08-16 15:46:38 -0300

    MVC controller e MVC repository adicionados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import conexao from '../infra/conexao.js'
+import SelecaoController from './app/controllers/SelecaoController.js'
 
 const app = express()
 
@@ -7,65 +7,10 @@ const app = express()
 app.use(express.json())
 
 // rotas
-app.get('/selecoes', (req, res) => {
-  const sql = "SELECT * FROM selecoes;"
-  conexao.query(sql, (erro, resultado) => {
-    if(erro) {
-      res.status(404).json({'erro': erro})
-    } else {
-      res.status(200).json(resultado)
-    }
-  })
-})
-
-app.get('/selecoes/:id', (req, res) => {
-  const id = req.params.id
-  const sql = "SELECT * FROM selecoes WHERE id=?;"
-  conexao.query(sql, id, (erro, resultado) => {
-    const linha = resultado[0]
-    if(erro) {
-      res.status(404).json({'erro': erro})
-    } else {
-      res.status(200).json(linha)
-    }
-  })
-})
-
-app.post('/selecoes', (req, res) => {
-  const selecao = req.body
-  const sql = "INSERT INTO selecoes SET ?;"
-  conexao.query(sql, selecao, (erro, resultado) => { 
-    if(erro) {
-      res.status(400).json({'erro': erro})
-    } else {
-      res.status(201).send(resultado)
-    }
-   })
-})
-
-app.delete('/selecoes/:id', (req, res) =>{
-  const id = req.params.id
-  const sql = `DELETE FROM selecoes WHERE id = ?`
-  conexao.query(sql, id, (erro, resultado) => {
-    if(erro) {
-      res.status(404).json({'erro': erro})
-    } else {
-      res.status(200).json(resultado)
-    }
-  })
-})
-
-app.put('/selecoes/:id', (req, res) =>{
-  const id = req.params.id
-  const selecao = req.body
-  const sql = "UPDATE selecoes SET ? WHERE id = ?"
-  conexao.query(sql, [selecao, id], (erro, resultado) => {
-    if(erro) {
-      res.status(404).json({'erro': erro})
-    } else {
-      res.status(200).json(resultado)
-    }
-  })
-})
-
+app.get('/selecoes', SelecaoController.index)
+app.get('/selecoes/:id', SelecaoController.show)
+app.post('/selecoes', SelecaoController.store)
+app.put('/selecoes/:id', SelecaoController.update)
+app.delete('/selecoes/:id', SelecaoController.delete)
+ 
 export default app
---
+++ src/app/controllers/SelecaoController.js
@@ -0,0 +1,37 @@
+import SelecaoRepository from '../repositories/SelecaoRepository.js'
+
+class SelecaoController {
+    
+    async index(req, res) {
+        const result = await SelecaoRepository.findAll()
+        res.json(result)
+      }
+
+    async show(req, res) {
+        const id = req.params.id
+        const result = await SelecaoRepository.findById(id)
+        res.json(result)
+      }
+
+    async store(req, res) {
+        const selecao = req.body
+        const result = await SelecaoRepository.create(selecao)
+        res.json(result)
+      }
+
+    async update(req, res) {
+        const id = req.params.id
+        const selecao = req.body
+        const result = await SelecaoRepository.update(selecao, id)
+        res.json(result)
+      }
+
+    async delete(req, res) {
+        const id = req.params.id
+        const result = await SelecaoRepository.delete(id)
+        res.json(result)
+      }
+}
+
+//padrao Singleton
+export default new SelecaoController()
